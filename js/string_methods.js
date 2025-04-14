97e515f9a2e0ae26ca13d6f08fd6ff8f88ed1189
Shamim Ahammad Rasel <shamimahammadrasel@gmail.com>
2025-04-15 02:09:30 +0600

    padStart() working well misunderstood the approach

--- js/string_methods.js
+++ js/string_methods.js
@@ -1,71 +1,69 @@
-// let text = "abraCadabr";
-// //          0123456789
+let text = "abraCadabr";
+//          0123456789
 
-// // string length -info (property)
-// console.log(text.length);
-
-
-// // string characters
-// // 1 -> with at(position)  method -- needs invokation
-// // 2 -> with charAt(position)  method -- needs invokation
-// // 3 -> with charCodeAt(position)  method -- needs invokation
-
-// let index = 5;
-
-// let ch = text.at(index);
-// console.log(ch);
-
-// ch = text.charAt(index);
-// console.log(ch);
-
-// ch = text.charCodeAt(index); // returns the ascii code number
-// console.log(ch);
-
-// // wecan directly use the index instead of method invokation
-// ch = text[index];
-// console.log(ch);
-
-// text[index] = 'A'; // no error but desn't works because javascript use immutable strings
-// console.log(text);
+// string length -info (property)
+console.log(text.length);
 
 
-// // ======== Extracting String parts =========
-// // slice(start, end)  --here the end in range is not included  (method -- needs invokation)
-// // substring(start, end)
-// // substr(start, length) --deprecated
+// string characters
+// 1 -> with at(position)  method -- needs invokation
+// 2 -> with charAt(position)  method -- needs invokation
+// 3 -> with charCodeAt(position)  method -- needs invokation
 
-// let sliced_part = text.slice(2, 5);
-// console.log(sliced_part); //we will get a string as text[2] + text[3] + text[4] in this case ="rac"
+let index = 5;
 
-// sliced_part = text.substring(-2);
-// console.log(sliced_part);
+let ch = text.at(index);
+console.log(ch);
 
-// // case conversion methods
+ch = text.charAt(index);
+console.log(ch);
 
-// let upper = text.toUpperCase();
-// console.log(upper);
+ch = text.charCodeAt(index); // returns the ascii code number
+console.log(ch);
 
-// let lower = text.toLowerCase();
-// console.log(lower);
+// wecan directly use the index instead of method invokation
+ch = text[index];
+console.log(ch);
 
-// // concat() join method
-
-// let fname = "Shamim";
-// let lname = "Hossain";
-
-// let fullName = fname.concat(" ", lname, " ", "Chowdhury");
-
-// console.log(fullName);
-
-// // trim method  --removes leading and tailing spaces
-// let mytext = "   Hello Boishakh ";
-// console.log(mytext.trim());
-// console.log(mytext.trim().length);  // --this will provide us length for the trimmed string
+text[index] = 'A'; // no error but desn't works because javascript use immutable strings
+console.log(text);
 
 
-// // string padding padStart() & padEnd()
-// let str = "****";
-// let newStr = str.padStart(2, "$");
-// console.log(newStr);
-let text = "5";
-text = text.padStart(4,"0");  //padStart() method is not working
+// ======== Extracting String parts =========
+// slice(start, end)  --here the end in range is not included  (method -- needs invokation)
+// substring(start, end)
+// substr(start, length) --deprecated
+
+let sliced_part = text.slice(2, 5);
+console.log(sliced_part); //we will get a string as text[2] + text[3] + text[4] in this case ="rac"
+
+sliced_part = text.substring(-2);
+console.log(sliced_part);
+
+// case conversion methods
+
+let upper = text.toUpperCase();
+console.log(upper);
+
+let lower = text.toLowerCase();
+console.log(lower);
+
+// concat() join method
+
+let fname = "Shamim";
+let lname = "Hossain";
+
+let fullName = fname.concat(" ", lname, " ", "Chowdhury");
+
+console.log(fullName);
+
+// trim method  --removes leading and tailing spaces
+let mytext = "   Hello Boishakh ";
+console.log(mytext.trim());
+console.log(mytext.trim().length);  // --this will provide us length for the trimmed string
+
+
+// string padding padStart() & padEnd()
+let str = "****";
+let newStr = str.padStart(2, "$");
+console.log(newStr);
