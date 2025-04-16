b040e03c7f5d806fc49ba280d9a987508cabb38a
Shamim Ahammad Rasel <shamimahammadrasel@gmail.com>
2025-04-17 00:24:55 +0600

    learning array search

--- js/array_search.js
+++ js/array_search.js
@@ -6,3 +6,35 @@ let first = numbers.findIndex((val) =>{
 
 
 console.log(first);
+
+const temp = [27, 28, 30, 40, 42, 52, 90];
+let pos = temp.findLastIndex(x => x > 40);
+console.log(pos);
+
+const points = [40, 100, 1, 5, 25, 10];
+
+points.sort(function(a, b){return a-b;});
+console.log(points);
+
+
+const myArr = [
+    {name:"X00",price:100 },
+    {name:"X01",price:100 },
+    {name:"X04",price:110 },
+    {name:"X05",price:110 },
+    {name:"X06",price:110 },
+    {name:"X02",price:100 },
+    {name:"X03",price:100 },
+    {name:"X07",price:110 }
+  ];
+
+myArr.sort(function(a, b){
+    return a.price - b.price;
+});
+
+console.log(myArr);
+
+
+points.forEach((val, index, array) => {
+    console.log(`${val}  =>  ${index}`);
+});
